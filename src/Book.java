7b8f5a6006a9044bd0b825d0b7deefeacad49498
artem-yanishev <janyshevtema79@gmail.com>
2025-05-19 22:22:10 +0300

    homework

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    String title;
+    Author author;
+    int yearOfPublication;
+
+    public Book(String title, Author author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
