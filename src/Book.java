58397750aa70f3df48fa27926831bd52079fd8c8
artem-yanishev <janyshevtema79@gmail.com>
2025-05-24 17:41:49 +0300

    homework 13 (corrected)

--- src/Book.java
+++ src/Book.java
@@ -36,11 +36,11 @@ public class Book {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+        return yearOfPublication == book.yearOfPublication && Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, author);
+        return Objects.hash(title, author, yearOfPublication);
     }
 }
